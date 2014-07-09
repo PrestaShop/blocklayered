e4d3d1389a0310030e05287365c01938e29307a6
Etienne Samson <samson.etienne@gmail.com>
2014-07-09 11:19:15 +0200

    [~] Deprecated : Tools::encrypt
    See PrestaShop/PrestaShop#1957.

--- blocklayered-price-indexer.php
+++ blocklayered-price-indexer.php
@@ -3,7 +3,7 @@
 include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/blocklayered.php');
 
-if (substr(Tools::encrypt('blocklayered/index'),0,10) != Tools::getValue('token') || !Module::isInstalled('blocklayered'))
+if (substr(Tools::hash('blocklayered/index'),0,10) != Tools::getValue('token') || !Module::isInstalled('blocklayered'))
 	die('Bad token');
 
 if (!Tools::getValue('ajax'))
